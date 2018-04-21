6d9080ccd279dcb25dac8f0ce59af655c9a606ec
Noob Pete <peteretep@live.dk>
2018-04-22 01:30:36 +0200

    highway, quest, and other

---
+++ Assets/QuestGiverScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiverScript : MonoBehaviour {
+    public GameObject topSecretPrefab;
+    public Transform[] placesToPlaceObjectives;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject o = Instantiate(topSecretPrefab);
+            o.transform.position = placesToPlaceObjectives[Random.Range(0, placesToPlaceObjectives.Length)].position;
+        }
+    }
+}
