f697f716ef0cb0c219469b5c4853fb304b4dae68
Noob Pete <peteretep@live.dk>
2018-04-21 22:53:32 +0200

    Change grass reflection, ramps, topsecret folder model and texture

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour {
+    public float rotateSpeed;
+    public Vector3 axis;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(axis, rotateSpeed * Time.deltaTime);
+	}
+}
