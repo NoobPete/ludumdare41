0b2d67e96186af6efd9d2a500240be011d1cd432
Noob Pete <peteretep@live.dk>
2018-04-24 01:27:14 +0200

    neato camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform postion;
+    public Transform lookAt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector3.Lerp(transform.position, postion.position, 0.4f);
+
+        transform.LookAt(lookAt.position);
+	}
+}
